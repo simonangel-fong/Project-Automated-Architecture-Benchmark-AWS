a04453e42fb23fbdd4a522a3882231b82db3bbb4
simonangel-fong <simonangelfong@gmail.com>
2025-11-24 00:25:13 -0500

    tf: add vpc

--- aws_baseline/aws_vpc_log.tf
+++ aws_baseline/aws_vpc_log.tf
@@ -59,5 +59,5 @@ resource "aws_flow_log" "vpc_flow_log" {
   iam_role_arn    = aws_iam_role.vpc_flow_log.arn
   log_destination = aws_cloudwatch_log_group.vpc_flow_log.arn
   traffic_type    = "ALL"
-  vpc_id          = aws_vpc.vpc.id
+  vpc_id          = aws_vpc.main.id
 }
